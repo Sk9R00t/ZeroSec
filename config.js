a7bf492f2aef97c44f337cf4a28d5341ad1de8f4
Sk9R00t <skyrootstudio12@gmail.com>
2026-03-08 19:58:35 +0600

    Add config.js for Discord bot and web dashboard configuration settings

---
+++ config.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+    discordToken: 'YOUR_DISCORD_TOKEN',
+    prefix: '!',
+    ownerID: 'YOUR_OWNER_ID',
+    dashboard: {
+        url: 'http://localhost:3000',
+        port: 3000,
+        secret: 'YOUR_SECRET',
+    },
+    otherConfig: {
+        // Add other configuration settings here
+    }
+};
